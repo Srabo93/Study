75c590e3a25745314a93fb12de0aac2dde4c1d66
Srabo93 <a.s-dev@web.de>
2023-04-29 20:01:39 +0200

    closures in rust

--- Rust/target/debug/deps/a21-287aecdeb31a7dfb.d
+++ Rust/target/debug/deps/a21-287aecdeb31a7dfb.d
@@ -1,5 +1,5 @@
-/home/armin/WebDev/Rust/rust-programming-for-beginners/target/debug/deps/a21-287aecdeb31a7dfb.rmeta: src/bin/a21.rs
+/home/armin/WebDev/Study/Rust/target/debug/deps/a21-287aecdeb31a7dfb.rmeta: src/bin/a21.rs
 
-/home/armin/WebDev/Rust/rust-programming-for-beginners/target/debug/deps/a21-287aecdeb31a7dfb.d: src/bin/a21.rs
+/home/armin/WebDev/Study/Rust/target/debug/deps/a21-287aecdeb31a7dfb.d: src/bin/a21.rs
 
 src/bin/a21.rs:
--- Rust/target/debug/deps/a21-bcacf3a15428a988.d
+++ Rust/target/debug/deps/a21-bcacf3a15428a988.d
@@ -1,5 +1,5 @@
-/home/armin/WebDev/Rust/rust-programming-for-beginners/target/debug/deps/a21-bcacf3a15428a988.rmeta: src/bin/a21.rs
+/home/armin/WebDev/Study/Rust/target/debug/deps/a21-bcacf3a15428a988.rmeta: src/bin/a21.rs
 
-/home/armin/WebDev/Rust/rust-programming-for-beginners/target/debug/deps/a21-bcacf3a15428a988.d: src/bin/a21.rs
+/home/armin/WebDev/Study/Rust/target/debug/deps/a21-bcacf3a15428a988.d: src/bin/a21.rs
 
 src/bin/a21.rs:
