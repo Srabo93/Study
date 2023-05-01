a782c65fa2158108c37c01a61dccc6fd47ec98ff
Srabo93 <a.s-dev@web.de>
2023-05-01 18:35:07 +0200

    wrote assertion tests

--- Rust/src/bin/a22.rs
+++ Rust/src/bin/a22.rs
@@ -23,12 +23,50 @@ fn clamp(n: i32, lower: i32, upper: i32) -> i32 {
 
 /// Divides a and b.
 fn div(a: i32, b: i32) -> Option<i32> {
+    if b == 0 {
+        return None;
+    }
     Some(a / b)
 }
 
 /// Takes two strings and places them immediately one after another.
 fn concat(first: &str, second: &str) -> String {
-    format!("{} {}", first, second)
+    format!("{}{}", first, second)
 }
 
+#[cfg(test)]
+mod test {
+    use crate::*;
+
+    #[test]
+    fn check_lower() {
+        let result = clamp(10, 100, 1000);
+        let expected = 100;
+        assert_eq!(result, expected, "should be 100");
+    }
+    #[test]
+    fn check_upper() {
+        let result = clamp(4000, 100, 1000);
+        let expected = 1000;
+        assert_eq!(result, expected, "should be 1000");
+    }
+    #[test]
+    fn check_div() {
+        let result = div(10, 2);
+        let expected = Some(5);
+        assert_eq!(result, expected, "should be 5");
+    }
+    #[test]
+    fn check_div_by_zero() {
+        let result = div(10, 0);
+        let expected = None;
+        assert_eq!(result, expected, "You cant divide by zero");
+    }
+    #[test]
+    fn check_concat() {
+        let result = concat("a", "b");
+        let expected = String::from("ab");
+        assert_eq!(result, expected, "should be ab");
+    }
+}
 fn main() {}
--- Rust/target/debug/deps/a22-aae2673d8f4b4e65.d
+++ Rust/target/debug/deps/a22-aae2673d8f4b4e65.d
@@ -1,5 +1,5 @@
-/home/armin/WebDev/Rust/rust-programming-for-beginners/target/debug/deps/a22-aae2673d8f4b4e65.rmeta: src/bin/a22.rs
+/home/armin/WebDev/Study/Rust/target/debug/deps/a22-aae2673d8f4b4e65.rmeta: src/bin/a22.rs
 
-/home/armin/WebDev/Rust/rust-programming-for-beginners/target/debug/deps/a22-aae2673d8f4b4e65.d: src/bin/a22.rs
+/home/armin/WebDev/Study/Rust/target/debug/deps/a22-aae2673d8f4b4e65.d: src/bin/a22.rs
 
 src/bin/a22.rs:
